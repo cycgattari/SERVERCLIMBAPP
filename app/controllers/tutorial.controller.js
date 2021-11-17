2fb06f807d32ab362934390cd4500854bd67e415
Maxi Gattari <cycgattari@hotmail.com>
2021-11-17 18:11:16 -0300

    dfg

--- app/controllers/tutorial.controller.js
+++ app/controllers/tutorial.controller.js
@@ -5,7 +5,7 @@ const Op = db.Sequelize.Op;
 // Create and Save a new Tutorial
 exports.create = (req, res) => {
   // Validate request
-  if (!req.body.murosector) {
+  if (!req.body) {
     res.status(400).send({
       message: "Content can not be empty!"
     });
--- server.js
+++ server.js
@@ -1,46 +1,30 @@
+const express = require("express");
+// const bodyParser = require("body-parser"); /* deprecated */
+const cors = require("cors");
 
-
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
 const app = express();
-const host = '0.0.0.0';
-const port = process.env.PORT || 5000;
 
-app.use(bodyParser.json());  /* bodyParser.json() is deprecated */
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
 
-app.use(cors());
-app.use(express.urlencoded({ extended: true }));
+app.use(cors(corsOptions));
 
-const db = require("./app/models");
+// parse requests of content-type - application/json
+app.use(express.json()); /* bodyParser.json() is deprecated */
 
-db.sequelize.sync({ force: true }).then(() => {
-  console.log("Drop and re-sync db.");
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true })); /* bodyParser.urlencoded() is deprecated */
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to bezkoder application." });
 });
 
+require("./app/routes/tutorial.routes.js")(app);
 
-app.get('/', (req, res) => {
-  res.send(`Hi! Server is listening on port ${port}`)
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
 });
-
-
-require("./app/routes/tutorial.routes")(app);
-
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('client/build'));
-}
-
-app.get('*', (request, response) => {
-	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-});
-
-// listen on the port
-
-app.listen(port, host, function() {
-  console.log("Server started.......");
-});
-
-  
-
-
-
